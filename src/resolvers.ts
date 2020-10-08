e7ca425e07b64a50add2130f4e47545579c1b46b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-10-08 16:07:09 +0100

    Fixed issue with the save method in resolvers.ts + added some notes about going forward

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,5 @@
-import { Note, INote } from './models/note'
-import { User, IUser } from './models/user'
-
+import { Note, INote } from "./models/note";
+import { User, IUser } from "./models/user";
 
 export default {
   Query: {
@@ -9,18 +8,20 @@ export default {
   },
 
   Mutation: {
-    createNote: (_:string, {sender, receiver, status, url}:INote) => {
-      const note = new Note({sender, receiver, status, url});
-      return note.save
-      // have code pushing this note ID to sender - sent array 
-      // reciever - recievedArray 
-
+    createNote: (_: string, { sender, receiver, status, url }: INote) => {
+      const note = new Note({ sender, receiver, status, url });
+      return note.save();
+      // have code pushing this note ID to sender - sent array
+      // reciever - recievedArray
     },
-    createUser: (_:string, {slackID}:IUser) => {
-      const senderNotes: Array<string> = []
-      const receiverNotes: Array<string> = []
-      const user = new User({slackID, senderNotes, receiverNotes});
-      return user.save()
-    }
-  }
-}
+    createUser: (_: string, { slackID }: IUser) => {
+      const senderNotes: Array<string> = [];
+      const receiverNotes: Array<string> = [];
+      const user = new User({ slackID, senderNotes, receiverNotes });
+      return user.save();
+    },
+  },
+};
+
+//We think for this to work, on createNote, the sender and receiver would need to be fished out of the
+//DB before we create a new note, as the sender + receiver are mapped to MongoID not SlackID

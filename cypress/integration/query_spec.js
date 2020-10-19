9c33e7fd38fd36e61ff772813e1ddb80ae4849c7
Tim <timcpb@hotmail.com>
2020-10-19 23:33:11 +0100

    remove unnecessary cy.task(getSchema)

--- cypress/integration/query_spec.js
+++ cypress/integration/query_spec.js
@@ -1,15 +1,9 @@
 describe("Server set up", () => {
   beforeEach(() => {
-    cy.task("getSchema").then((schema) => {
-      cy.mockGraphql({
-        schema,
-      });
-    });
     cy.exec("yarn run db:drop");
   });
 
-  it("connects to the graphql server and prints", () => {
-    cy.log("Hello World");
+  it("returns users when user query is fired", () => {
     const query = `
       {
         users{
@@ -17,15 +11,17 @@ describe("Server set up", () => {
         }
       }
     `;
-    cy.task("addUser", { slackID: "TestSender" });
-    cy.task("addUser", { slackID: "TestReceiver" });
+    cy.task("addUser", { slackID: "TestUser" });
+    cy.task("addUser", { slackID: "TestUser2" });
     cy.request({
       method: "post",
       url: "http://localhost:4000/graphql/", // graphql endpoint
       body: { query }, // or { query: query } depending if you are writing with es6
       failOnStatusCode: false, // not a must but in case the fail code is not 200 / 400
     }).then((res) => {
-      expect(res.body.data.users[0].slackID).to.equal("TestSender");
+      expect(res.body.data.users[0].slackID).to.equal("TestUser");
+      expect(res.body.data.users[1].slackID).to.equal("TestUser2");
     });
   });
 });
+
